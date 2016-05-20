2b03ca3e085e709ce6921ba08c91dc4151d66856
unknown <www.darmagh@mail.ru>
2016-05-20 23:04:06 +0300

    lab7 without tests

---
+++ FindMaxEx_lab7.1.3/FindMaxEx_lab7.1.3/FindMaxEx.h
@@ -0,0 +1,13 @@
+#pragma once
+template < typename T, typename Less>
+bool FindMax(std::vector<T> const& arr, T& maxValue, Less const& less)
+{
+	for (auto &it: arr)
+	{
+		if(less(maxValue, it))
+		{
+			maxValue = it;
+		}
+	}
+	return true;
+}
